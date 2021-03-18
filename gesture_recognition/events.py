3b6d611c37d551d03b2ef6ddf8553c1d374a623c
Josh Clemons <clemonsjoshua6@gmail.com>
2021-03-17 23:04:28 -0700

    implemented celery

--- gesture_recognition/events.py
+++ gesture_recognition/events.py
@@ -4,7 +4,7 @@ from . import db, socketio, celery
 from . import featurizer, predictor
 from .auth import verify_token
 from flask import g, session, request
-from flask_socketio import emit 
+from flask_socketio import emit
 from PIL import Image
 import numpy as np
 import cv2
@@ -13,6 +13,8 @@ import os
 import datetime
 from config import Config
 import json
+import copy
+from celery.contrib import rdb
 
 @socketio.on('request_stats')
 def stats():
@@ -44,16 +46,80 @@ def stats():
     emit('stats', fig_dict)
 
 @celery.task
-def post_pred(user_id, output_dict):
-    """Celery task that posts a prediction."""
+def post_error(output_dict):
+    """Celery task that emits invalid login error to user
+    
+    Args:
+        output_dict (dict): Dictionary containing items to emit to user
+    """
 
     from .wsgi_aux import app
     with app.app_context():
-        user = User.query.get(user_id)
-        if user is None:
-            return
+        socketio.emit('response_image', output_dict, room=output_dict['room']) # broadcast to all users 
 
-        emit('response_image', output_dict) 
+@celery.task
+def post_pred(user_id, frame_processed, frame_orig, frame_dict, output_dict):
+    """Celery task that handles image storage and database push operations and emits processed image and prediction to user
+    
+    Args:
+        user_id (int): Numerical user ID 
+        frame_orig (array): User's original image
+        frame_processed (array): User's processed image with or without background remove
+        frame_dict (dict): Dictionary containing image information to push to database
+        output_dict (dict): Dictionary containing items to emit to user
+    """
+    
+    from .wsgi_aux import app
+    with app.app_context():
+        # prepare image for export
+        output_bin = cv2.imencode('.jpg', frame_processed)[1].tobytes()
+        encoded_output = base64.b64encode(output_bin).decode()
+        output_dict['train_image'] = encoded_output
+
+        # save original and processed images in directory correponding to user
+        root_dir = Config.IMAGE_DIRECTORY
+        image_path = frame_dict['instance'] + '.jpg'
+
+        # save original images
+        orig_dir = os.path.join(root_dir, 'original')
+        user_dir = os.path.join(orig_dir, str(user_id))
+        raw_path = os.path.join(user_dir, image_path)
+        if cv2.imwrite(raw_path, frame_orig) == False: 
+            os.mkdir(user_dir)
+            print(f'[INFO] Created directory for original images from user {user_id}.')
+            cv2.imwrite(raw_path, frame_orig)
+
+        # save processed images
+        if frame_dict['pred_time'] > 0:
+            processed_dir = os.path.join(root_dir, 'processed')
+            user_dir = os.path.join(processed_dir, str(user_id))
+            processed_path = os.path.join(user_dir, image_path)
+            if cv2.imwrite(processed_path, frame_processed) == False: 
+                os.mkdir(user_dir)
+                print(f'[INFO] Created directory for processed images from user {user_id}.')
+                cv2.imwrite(processed_path, frame_processed)
+        else:
+            processed_path = None
+
+        # store results in database
+        now = datetime.datetime.now()
+        date = now.strftime("%Y-%m-%d %H:%M:%S") 
+        
+        frame_dict['date'] = date
+        frame_dict['user_id'] = user_id
+        frame_dict['root_dir'] = root_dir
+        frame_dict['raw_path'] = raw_path
+        frame_dict['processed_path'] = processed_path
+
+        frame = Frame.create(frame_dict, user=User.query.get(user_id))
+        try:
+            db.session.add(frame)
+            db.session.commit()
+        except:
+            print('[WARNING] Duplicate instance. Unable to commit frame to database.')
+        db.session.remove()
+        
+        socketio.emit('response_image', output_dict, room=output_dict['room']) # broadcast to all users 
 
 @socketio.on('post_image')
 def process_image(data): 
@@ -62,15 +128,16 @@ def process_image(data):
     Args:
         data (dict): Dictionary containing client data: streamed webcam frame (byte array), token (str), input command (str), and ground-truth gesture (str)
     """
-
+    
     token = data['token']    
     result = verify_token(token, add_to_session=True)
+    output_dict = {}
     if result is False:
-        output_dict = {}
         output_dict['train_image'] = ''
         output_dict['label'] = 'Invalid login attempt.'
         output_dict['command'] = ''
-        emit('response_image', output_dict) 
+        output_dict['room'] = request.sid
+        post_error.apply_async(args=(output_dict))
     elif g.current_user:
         user_id = g.current_user.id
         user = User.query.get(user_id)
@@ -83,20 +150,21 @@ def process_image(data):
             session['background'] = 0
         if 'frame_count' not in session_keys:
             session['frame_count'] = 0
-        if 'frame_dims' not in session_keys:
-            session['frame_dims'] = 0
         if 'pause_count' not in session_keys:
             session['pause_count'] = 0
         if 'start_countdown' not in session_keys:
             session['start_countdown'] = False
 
-        # frame metadata 
+        # frame info for db 
         session['frame_count'] += 1  
         session_id = user.num_logins
         instance = str(user_id) + '_' + str(session_id) + '_' + str(session['frame_count'])
-        now = datetime.datetime.now()
-        date = now.strftime("%Y-%m-%d %H:%M:%S") 
         ip_address = request.remote_addr
+        frame_dict = {}
+        frame_dict['instance'] = instance
+        frame_dict['session_id'] = session_id
+        frame_dict['frame_count'] = session['frame_count']
+        frame_dict['ip_address'] = ip_address
 
         # get webcam image
         image_file = 'temp_file'
@@ -105,90 +173,37 @@ def process_image(data):
         image_object = Image.open(image_file)
         frame_orig = cv2.cvtColor(np.array(image_object), cv2.COLOR_RGB2BGR)
 
-        # process image
+        # process image. would like to place in Celery task but client session dictionary not accessible in asynchronous task, and bgModel object non-serializable 
         user_command = data['command']
         if user_command != 'r' and user_command != 'b':
             if session['start_countdown']:
                 session['pause_count']+=1 
-                if frame_orig.shape != session['frame_dims']:
-                    error_message = "Frame must have same dimensions as background image."
-                    emit_error(error_message)
             [frame_processed, frame_prediction] = featurizer.process(frame_orig, session['background'])
         elif user_command == 'b':
             [frame_processed, frame_prediction] = featurizer.process(frame_orig, session['background'])
             bgModel = featurizer.set_background(frame_orig)
             session['background'] = bgModel
-            session['frame_dims'] = frame_orig.shape
             session['start_countdown'] = True
         elif user_command == 'r':
             [frame_processed, frame_prediction] = featurizer.process(frame_orig, session['background'])
             session['background'] = 0 
-            session['frame_dims'] = 0
             session['start_countdown'] = False
-            session['pause_count'] = 0    
+            session['pause_count'] = 0   
 
-        # predict gesture
+        # predict gesture. would like to place in Celery task but requires implementing Tensorflow Serving 
         true_gest = data['gesture']
-        [label, pred_gest, pred_conf, pred_time] = predictor.predict_gesture(frame_prediction, true_gest, session)
-
-        # package results in dictionary
-        output_bin = cv2.imencode('.jpg', frame_processed)[1].tobytes()
-        encoded_output = base64.b64encode(output_bin).decode()
-        output_dict = {}
-        output_dict['train_image'] = encoded_output
+        [label, pred_gest, pred_conf, pred_time] = predictor.predict_gesture(frame_prediction, true_gest, session) # 
         output_dict['label'] = label
+
+        # update dictionaries before sending to celery task 
         output_dict['command'] = user_command
-
-        # save original and processed images in directory correponding to user
-        root_dir = Config.IMAGE_DIRECTORY
-        image_path = instance + '.jpg'
-
-        # save original images
-        orig_dir = os.path.join(root_dir, 'original')
-        user_dir = os.path.join(orig_dir, str(user_id))
-        raw_path = os.path.join(user_dir, image_path)
-        if cv2.imwrite(raw_path, frame_orig) == False: 
-            os.mkdir(user_dir)
-            print(f'[INFO] Created directory for original images from user {user_id}.')
-            cv2.imwrite(raw_path, frame_orig)
-
-        # save processed images
-        if pred_time > 0:
-            processed_dir = os.path.join(root_dir, 'processed')
-            user_dir = os.path.join(processed_dir, str(user_id))
-            processed_path = os.path.join(user_dir, image_path)
-            if cv2.imwrite(processed_path, frame_processed) == False: 
-                os.mkdir(user_dir)
-                print(f'[INFO] Created directory for processed images from user {user_id}.')
-                cv2.imwrite(processed_path, frame_processed)
-        else:
-            processed_path = None
-
-        # store results in database
-        frame_dict = {}
-        frame_dict['instance'] = instance
-        frame_dict['date'] = date
-        frame_dict['user_id'] = user_id
-        frame_dict['session_id'] = session_id
-        frame_dict['frame_count'] = session['frame_count']
-        frame_dict['ip_address'] = ip_address
-        frame_dict['root_dir'] = root_dir
-        frame_dict['raw_path'] = raw_path
-        frame_dict['processed_path'] = processed_path
+        output_dict['room'] = request.sid   
         frame_dict['true_gest'] = true_gest
         frame_dict['pred_gest'] = pred_gest
         frame_dict['pred_conf'] = pred_conf
         frame_dict['pred_time'] = pred_time
-        frame = Frame.create(frame_dict, user=user)
-        try:
-            db.session.add(frame)
-            db.session.commit()
-        except:
-            print('[WARNING] Duplicate instance. Unable to commit frame to database.')
-        db.session.remove()
 
-        # emit('response_image', output_dict) 
-        post_pred.apply_async(args=(user_id, output_dict))
+        post_pred.apply_async(args=(user_id, frame_processed, frame_orig, frame_dict, output_dict))
 
 @socketio.on('disconnect')
 def on_disconnect():
